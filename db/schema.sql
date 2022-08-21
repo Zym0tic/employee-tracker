65c9ecbd3636012093b591e876207cdfdd3d0a0e
zym0tic <lukerozon@gmail.com>
2022-08-21 12:20:26 -0400

    added to tables, added package json with mysql2 and inquirer

--- db/schema.sql
+++ db/schema.sql
@@ -7,21 +7,25 @@ USE company_db;
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    Last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    Last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
-    id INT,
-    name VARCHAR(30),
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
 );
+
 
