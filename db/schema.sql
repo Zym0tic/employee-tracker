7fdca7a18c8be3efe6d5e989e757d81e80490a06
zym0tic <lukerozon@gmail.com>
2022-08-22 21:56:39 -0400

    addeds seeds

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,13 @@ USE company_db;
 
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
